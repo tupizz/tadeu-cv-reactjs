8e06c876a2485b0a52f7826e293ae7b012222a38
Tadeu Tupinambá <tadeu.tupiz@gmail.com>
2019-12-22 17:07:17 -0300

    Updates

---
+++ precache-manifest.04df6714df8d5b29faa12b098dfebdd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db1ecd38f90b0e88a5da9cea8a22d036",
+    "url": "/react-online-cv/index.html"
+  },
+  {
+    "revision": "2f94c1829e6b50dc7499",
+    "url": "/react-online-cv/static/css/main.fd7a2511.chunk.css"
+  },
+  {
+    "revision": "a455e4fb0165fff519ba",
+    "url": "/react-online-cv/static/js/2.09e93a96.chunk.js"
+  },
+  {
+    "revision": "f05bfc683a175306602e6f609a238cdb",
+    "url": "/react-online-cv/static/js/2.09e93a96.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f94c1829e6b50dc7499",
+    "url": "/react-online-cv/static/js/main.9376cc62.chunk.js"
+  },
+  {
+    "revision": "4e88cde1a2363e29dca9",
+    "url": "/react-online-cv/static/js/runtime-main.79accfa4.js"
+  }
+]);
